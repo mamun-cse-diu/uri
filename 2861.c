4bff971935cddbb125cb74f3e669c7923bf6443a
MD.AL-MAMUN <59719431+mamun-cse-diu@users.noreply.github.com>
2020-07-15 20:53:09 +0600

    Add files via upload

---
+++ 2861.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    char str[300],temp;
+    int t,i;
+    scanf("%d",&t);
+    while(t!=0){
+        scanf("%c",&temp);
+        fgets(str,300,stdin);
+        printf("gzuz\n");
+        t--;
+    }
+    return 0;
+}
